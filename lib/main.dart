af8a3fc19dccfc90aa49433c501dc27d507065dd
Dimitris Veneris <82315853+DVeneris@users.noreply.github.com>
2023-02-21 22:16:07 +0200

    commit

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+//  int _counter = 0;
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: AppBar().preferredSize.height + 40,
+        centerTitle: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(15),
+          ),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+        ],
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+        title: const TextField(
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Color.fromARGB(255, 87, 171, 240),
+              suffixIcon: Icon(Icons.search),
+              hintText: "Search Data...",
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 0, color: Colors.blue),
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 0, color: Colors.blue),
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
+              hintStyle: TextStyle(
+                color: Colors.white60,
+              ),
+            )),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                child: ListView(scrollDirection: Axis.vertical, children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 20.0,
+                  left: 20.0,
+                  right: 20.0,
+                ),
+                child: Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.blue[50],
+                  ),
+                  child: Column(children: []),
+                ),
+              ),
+            ]))
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+        backgroundColor: Colors.blue[100],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
